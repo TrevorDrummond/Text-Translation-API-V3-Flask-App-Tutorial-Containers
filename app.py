8ce27ebb4c3c8c107f77626fa5a65c86f31a8465
TrevorDrummond <87454950+TrevorDrummond@users.noreply.github.com>
2022-01-19 15:39:07 +1100

    Containers (#1)
    * Modified to support translate containers
    
    Co-authored-by: Trevor Drummond <tdrummond@microsoft.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, jsonify, request
-import translate, sentiment, synthesize
+import translate, sentiment
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -8,12 +8,20 @@ app.config['JSON_AS_ASCII'] = False
 def index():
     return render_template('index.html')
 
+@app.route('/detect-text', methods=['POST'])
+def detect_text():
+    data = request.get_json()
+    text_input = data['text']
+    response = translate.get_detectlanguage(text_input)
+    return jsonify(response)
+
 @app.route('/translate-text', methods=['POST'])
 def translate_text():
     data = request.get_json()
     text_input = data['text']
     translation_output = data['to']
-    response = translate.get_translation(text_input, translation_output)
+    translation_input = data['from']
+    response = translate.get_translation(text_input, translation_output, translation_input)
     return jsonify(response)
 
 @app.route('/sentiment-analysis', methods=['POST'])
@@ -25,13 +33,3 @@ def sentiment_analysis():
     output_lang =  data['outputLanguage']
     response = sentiment.get_sentiment(input_text, input_lang, output_text, output_lang)
     return jsonify(response)
-
-@app.route('/text-to-speech', methods=['POST'])
-def text_to_speech():
-    data = request.get_json()
-    text_input = data['text']
-    voice_font = data['voice']
-    tts = synthesize.TextToSpeech(text_input, voice_font)
-    tts.get_token()
-    audio_response = tts.save_audio()
-    return audio_response
--- translate.py
+++ translate.py
@@ -1,21 +1,25 @@
 import os, requests, uuid, json
+from urllib import response
 
-# Don't forget to replace with your Cog Services subscription key!
-# If you prefer to use environment variables, see Extra Credit for more info.
-subscription_key = 'YOUR_TRANSLATOR_TEXT_SUBSCRIPTION_KEY'
+#UPDATE THESE VALUES TO THE CORRECT CONTAINER URLs
+translate_base_url = 'http://localhost:5002'
+detect_base_url = 'http://localhost:5001'
 
-# Our Flask route will supply two arguments: text_input and language_output.
-# When the translate text button is pressed in our Flask app, the Ajax request
-# will grab these values from our web app, and use them in the request.
+# No Subscription key required for a local cog services container
+
+# Our Flask route will supply three arguments: text_input, language_output and language_input.
+# When the translate text button is pressed in our Flask app, 2 Ajax calls are made. 
+# First to get_detectlanguage to determine the language_input, then to get_translation.
+# The Ajax request will grab these values from our web app, and use them in the request.
 # See main.js for Ajax calls.
-def get_translation(text_input, language_output):
-    base_url = 'https://api.cognitive.microsofttranslator.com'
+def get_translation(text_input, language_output, language_input):
+    translate_base_url = 'http://localhost:5002'
     path = '/translate?api-version=3.0'
-    params = '&to=' + language_output
-    constructed_url = base_url + path + params
+    params = '&from=' + language_input + '&to=' + language_output
+    constructed_url = translate_base_url + path + params
 
     headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key,
+        #'Ocp-Apim-Subscription-Key': subscription_key,
         'Content-type': 'application/json',
         'X-ClientTraceId': str(uuid.uuid4())
     }
@@ -26,3 +30,20 @@ def get_translation(text_input, language_output):
     }]
     response = requests.post(constructed_url, headers=headers, json=body)
     return response.json()
+
+def get_detectlanguage(text_input):    
+    path = '/text/analytics/v3.0/languages'
+    constructed_url = detect_base_url + path
+#We're only detecting a single document as part of this call even though the service can do multiple.
+    headers = {'Content-Type' : 'application/json'}
+    body = { 'documents' : [{
+        'id' : '1',
+        'text' : text_input
+    }]}
+
+    response = requests.post(constructed_url, headers=headers, json=body)
+    if response.status_code == 200:
+        responseObject = json.loads(response.text)
+        #Return the first detected result
+        return responseObject['documents'][0]
+        
